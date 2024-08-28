f08f249901b4078718b9fd98e9898e9ac6e85b38
Mubin Shafeek Shaikh <mubinshaikh@AD.NSEIT.COM>
2024-08-28 15:15:10 +0530

    change in app.py and deleted db

--- Model deployment using Flask/app.py
+++ Model deployment using Flask/app.py
@@ -143,36 +143,6 @@ if __name__ == "__main__":
 
 app.config["SECRET_KEY"] = "secretkey"
 
-# @app.route("/register", methods=['GET', 'POST'])
-# def register():
-#     if request.method == "POST":
-#         # check if the form is valid
-
-#             if not request.form.get("email") or not request.form.get("password") or not request.form.get("confirmation"):
-#                 return "please fill out all fields"
-
-#             if request.form.get("password") != request.form.get("confirmation"):
-#                 return "password confirmation doesn't match password"
-
-#             # check if email exist in the database
-#             exist = c.execute("SELECT * FROM users WHERE email=:email", {"email": request.form.get("email")}).fetchall()
-
-#             if len(exist) != 0:
-#                 return "user already registered"
-
-#             # hash the password
-#             pwhash = generate_password_hash(request.form.get("password"), method="pbkdf2:sha256", salt_length=8)
-
-#             # insert the row
-#             c.execute("INSERT INTO users (email, password) VALUES (:email, :password)", {"email": request.form.get("email"), "password": pwhash})
-#             conn.commit()
-
-#             # return success
-#             flash("registered successfully!")
-#             return render_template('login.html')
-#     else:
-#         return render_template('register.html')
-
 
 
 
