0899533b8e9fd227f3ab84e87a031e1f8e3f3579
Mubin Shafeek Shaikh <mubinshaikh@AD.NSEIT.COM>
2024-08-28 15:11:27 +0530

    added login and logout feature

--- Model deployment using Flask/app.py
+++ Model deployment using Flask/app.py
@@ -1,16 +1,118 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask, flash,render_template,url_for,request
 import joblib
 import re
 import string
 import pandas as pd
+from flask import Flask, url_for, render_template, request, redirect, session
+from werkzeug.security import check_password_hash, generate_password_hash
+import sqlite3
+import secrets
 
 
 app = Flask(__name__)
-Model = joblib.load('C:/Users/Rabia/Desktop/Newss/Fake_news_Detection/model.pkl')
+Model = joblib.load('C:/Users/mubinshaikh/Desktop/fake content identifications/fake_content_dections/model.pkl')
+
+secret_key = secrets.token_hex(24)
+app.secret_key = str(secret_key)
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/login")
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    context = {
+        'authenticated': {
+            'user': False  # Set to a truthy value if the user is authenticated
+        }
+    }
+    return render_template("login.html", **context)
+
+# @app.route('/login')
+# def login():
+#     return render_template("login.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+        # check the form is valid
+        if not request.form.get("email") or not request.form.get("password"):
+            return "please fill out all required fields"
+
+        # check if email exist in the database
+        user = c.execute("SELECT * FROM users WHERE email=:email", {"email": request.form.get("email")}).fetchall()
+
+        if len(user) != 1:
+            return "you didn't register"
+
+        # check the password is same to password hash
+        pwhash = user[0][2]
+        if check_password_hash(pwhash, request.form.get("password")) == False:
+            return "wrong password"
+
+        # login the user using session
+        session["user_id"] = user[0][0]
+
+        # return success
+        return render_template("index.html")
+
+    else:
+        context = {
+        'authenticated': {
+            'user': False  # Set to a truthy value if the user is authenticated
+        }
+        }
+        return render_template("login.html", **context)
+
+
+conn = sqlite3.connect("db.sqlite3", check_same_thread=False)
+c = conn.cursor()
+
+c.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+    )
+''')
+
+@app.route('/register',  methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
+        # check if the form is valid
+
+        if not request.form.get("email") or not request.form.get("password") or not request.form.get("confirmation"):
+            return "please fill out all fields"
+
+        if request.form.get("password") != request.form.get("confirmation"):
+            return "password confirmation doesn't match password"
+
+        # check if email exist in the database
+        exist = c.execute("SELECT * FROM users WHERE email=:email", {"email": request.form.get("email")}).fetchall()
+
+        if len(exist) != 0:
+            return "user already registered"
+
+        # hash the password
+        pwhash = generate_password_hash(request.form.get("password"), method="pbkdf2:sha256", salt_length=8)
+
+        # insert the row
+        c.execute("INSERT INTO users (email, password) VALUES (:email, :password)", {"email": request.form.get("email"), "password": pwhash})
+        conn.commit()
+
+        # return success
+        flash("registered successfully!")
+        return redirect('/login')
+    else:
+        context = {
+        'authenticated': {
+            'user': True  # Set to a truthy value if the user is authenticated
+        }
+        }
+        return render_template('register.html', **context)
 
 def wordpre(text):
     text = text.lower()
@@ -23,7 +125,7 @@ def wordpre(text):
     text = re.sub(r'\w*\d\w*', '', text)
     return text
 
-@app.route('/',methods=['POST'])
+@app.route('/index',methods=['POST'])
 def pre():
     if request.method == 'POST':
         txt = request.form['txt']
@@ -36,3 +138,41 @@ def pre():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+app.config["SECRET_KEY"] = "secretkey"
+
+# @app.route("/register", methods=['GET', 'POST'])
+# def register():
+#     if request.method == "POST":
+#         # check if the form is valid
+
+#             if not request.form.get("email") or not request.form.get("password") or not request.form.get("confirmation"):
+#                 return "please fill out all fields"
+
+#             if request.form.get("password") != request.form.get("confirmation"):
+#                 return "password confirmation doesn't match password"
+
+#             # check if email exist in the database
+#             exist = c.execute("SELECT * FROM users WHERE email=:email", {"email": request.form.get("email")}).fetchall()
+
+#             if len(exist) != 0:
+#                 return "user already registered"
+
+#             # hash the password
+#             pwhash = generate_password_hash(request.form.get("password"), method="pbkdf2:sha256", salt_length=8)
+
+#             # insert the row
+#             c.execute("INSERT INTO users (email, password) VALUES (:email, :password)", {"email": request.form.get("email"), "password": pwhash})
+#             conn.commit()
+
+#             # return success
+#             flash("registered successfully!")
+#             return render_template('login.html')
+#     else:
+#         return render_template('register.html')
+
+
+
+
